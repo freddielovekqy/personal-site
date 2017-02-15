54b8c520a20b4f5f0ad339288b6ce99e6894726e
freddie <freddiewang@outlook.com>
2017-02-15 22:09:49 +0800

    创建博客以及博客管理页面的tab页

---
+++ webapp/js/blog/CreateBlogController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by freddie on 2017/2/15.
+ */
+var createBlogModule = angular.module('createBlog', []);
+
+createBlogModule.controller('CreateBlogController', ['$scope',
+    function ($scope) {
+
+    }
+]);
